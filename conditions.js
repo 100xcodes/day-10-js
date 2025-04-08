4a194d4e332e01cb480031bb6659547e9a68436f
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-04-09 00:27:03 +0530

    Push to GitHub

---
+++ conditions.js
@@ -0,0 +1,9 @@
+let age = 0;
+
+if (age <= 0) {
+  console.log("Invalid age provided");
+} else if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are not an adult");
+}
---
+++ functions.js
@@ -0,0 +1,14 @@
+let age1 = 20;
+let age2 = 17;
+
+function checkAge(age) {
+  if (age >= 18) {
+    console.log("You are an adult");
+  } else {
+    console.log("You are not an adult");
+  }
+}
+
+checkAge(age1);
+checkAge(age2);
+checkAge(18);
